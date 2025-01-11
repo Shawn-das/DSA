55e652e3f1bb652db2d41aec40cd19640b764876
Shawn das Shachin <135173800+shawningithub@users.noreply.github.com>
2025-01-11 22:44:13 +0600

    new start

---
+++ intro.cpp
@@ -0,0 +1,17 @@
+// Author: Shawn Das Shachin-->(shawn_das)
+
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+#define pb push_back
+#define mod 1000000007
+#define srt(v) sort(v.begin(),v.end())
+#define rsrt(v) sort(v.rbegin(),v.rend())
+#define OPTIMIZE_IO ios::sync_with_stdio(false); cin.tie(nullptr);
+
+int main() {
+    OPTIMIZE_IO;
+    
+    return 0;
+}
